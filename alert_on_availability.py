a671a9947fdc879bc7614376f5864145e277af95
JyothsnaGaliHx <JyothsnaG@hexaware.com>
2021-05-11 23:11:25 +0530

    working

--- alert_on_availability.py
+++ alert_on_availability.py
@@ -7,13 +7,13 @@ from email.mime.text import MIMEText
   
 # api-endpoint
 #URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin"
-URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict"
-  
+URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict"#?district_id=265&date=11-05-2021
+
 # params
-#pincode = "<insert_pincode>"
-district_id="265" #Bangalore Urban
+#pincode = "560076"
+district="265"
 # insert the start dates of the weeks you want to check vaccine availability for
-dates = ["12-05-2021", "13-05-2021", "14-05-2021","15-05-2021", "16-05-2021", "17-05-2021","12-05-2021", "18-05-2021", "19-05-2021","20-05-2021"]
+dates = ["11-05-2021", "12-05-2021", "13-05-2021"]
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 # sender's credentials
@@ -29,16 +29,20 @@ recheck_after = 60
 
 while True:
 	for date in dates:
+		PARAMS = {'district_id':district, 'date':date}
 		print(date)
-		PARAMS = {'district_id':district_id, 'date':date}
 		r = requests.get(url = URL, params = PARAMS, headers = headers)
-		  
+		print(r)
 		data = r.json()
+		#print(data)
 		for center in data['centers']:
 			name = center['name']
 			addr = center['address']
 			for session in center['sessions']:
 				available_capacity = session['available_capacity']
+				if available_capacity >0 :
+					print(name)
+					print(available_capacity)
 				min_age_limit = session['min_age_limit']
 				vaccine = session['vaccine']
 				slot_date = session['date']
